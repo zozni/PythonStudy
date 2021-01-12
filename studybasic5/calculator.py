400899c12b039b365aa8fbe697268e806fad0f7d
zozni <thekey1027@naver.com>
2021-01-12 23:41:13 +0900

    상속

--- studybasic5/calculator.py
+++ studybasic5/calculator.py
@@ -8,10 +8,26 @@ class FourCal:   #부모 클래스
     def add(self):
         result = self.first + self.second
         return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
+
+class SafeFourCal(FourCal): #자식 클래스 div가 부모한테 있는데 이걸 한번더 정의함.
+    def div(self):           #자식이 우선시 된다. 그냥 나누는게 아니라 0으로 나누면 안되니까가 추가됨.
+        if self.second == 0:
+            return 0
+        else:
+            return self.first/self.second
 
 class MoreFourCal(FourCal): #자식 클래스
-    def pow(self):
-        result = self.first ** self.second
+    def pow(self):  #부모에게 없는 기능
+        result = self.first ** self.second #지수
         return result
 a = MoreFourCal(4, 2)
-print(a.add())
+print(a.pow())
